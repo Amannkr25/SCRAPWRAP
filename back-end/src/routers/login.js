80e775065accadf74ff7e6d777db0bd8283e764e
shashwat Tejaswi <shashwatteju06@gmail.com>
2025-04-29 15:53:51 +0530

    add login with create statefull session id send as a cookies

--- back-end/src/routers/login.js
+++ back-end/src/routers/login.js
@@ -1,13 +1,43 @@
 const express =require("express");
-
+const {Users} = require("../models/user")
 const loginRoute=express.Router();
+const bcrypt=require("bcrypt");
+const {setSession,getSession}=require("../auth/sessionId")
+import { v4 as uuidv4 } from 'uuid';
 
 
-loginRoute.post('/',(req,res)=>{
 
-    const {username,password}=req.body;
+
+loginRoute.post('/',async(req,res)=>{
+
+    const {email,password}=req.body;
+
+    const user=await Users.findOne({email});
+
+    if(!user)
+    {
+        res.status(404).json({status:false,messege:"user not found"});
+    }
+
+    const isPasswordCorrect =await bcrypt.compare(password,user.password);
+
+    if(!isPasswordCorrect)
+    {
+        res.status(404).json({status:false,messege:"wrong password"});
+    }
+    const sessionToken = uuidv4();
+    setSession(sessionToken,user);
+    
+    res.cookie('session_token', sessionToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge:  60 * 60 * 1000,
+      });
+    res.status(200).json({status:true,messege:"login successful",user:user})
 
     
+
+
 
 
 
